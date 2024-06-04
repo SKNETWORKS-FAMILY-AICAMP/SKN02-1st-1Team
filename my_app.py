6f5f8817a6e68606daa72859a548c25d6af21015
pkh1339 <pkh526@seoultech.ac.kr>
2024-06-04 14:40:31 +0900

    240604_kh_1년 등록현황 데이터 재업로드 및 my_app 수정

--- my_app.py
+++ my_app.py
@@ -1,61 +1,137 @@
-import streamlit as st
-from streamlit_folium import st_folium
 import folium
-import pandas as pd
+from streamlit_folium import folium_static
 from streamlit_option_menu import option_menu
+from collections import Counter
+import matplotlib.pyplot as plt
+import json
+import pandas as pd
+import streamlit as st
 
-st.title('자동차 현황 및 스피드메이트 영업점 분석')
+### 변수 ###
+
+
+
+### CLASS DECLARE ###
+class View:
+    def __init__(self, qna):
+        self.qna = qna
+
+    # def ViewFAQ(self, opt):
+    #     with st.container():
+    #         result = self.qna[self.qna['분류'] == opt]  
+
+    #         for i in range(len(result)):            
+    #             with st.expander(result['질문'][i]):
+    #                 st.write(
+    #                 result['답변'][i]
+    #             )
+    
+
+    def ViewAllFAQ(self):
+        with st.container():
+            for i in range(len(self.qna['질문'])):
+                with st.expander(self.qna['질문'][i]):
+                    st.write(
+                    self.qna['답변'][i]
+                    )
+                    
+##########################
+
+#####################################################33
+# count_res = Counter(df['주소'])
+df3 = pd.read_csv('./data/autooasis_store.csv')
+count_res = Counter(df3['address_code'])
+
+# 대한민국 시,군,구의 경계 GeoJSON 파일 경로
+geojson_path = 'TL_SCCO_SIG.json' 
+
+# 시,군,구 경계 GeoJSON 파일 로드
+geo_data = json.load(open(geojson_path, encoding='utf-8'))
+
+# 지점이 없는 시,군,구는 GeoJSON 파일에서 데이터 제거
+for feature in geo_data['features']:
+    region_name = feature['properties']['SIG_CD']
+    if region_name not in count_res:
+        geo_data['features'].remove(feature)
+
+# Folium 지도 초기화
+
+m = folium.Map(location=[36.5, 127.5], zoom_start=6)
+
+folium.GeoJson(
+    geo_data,
+    name='지역구'
+).add_to(m)
+
+folium.Choropleth(geo_data=geo_data,
+             data=count_res  , 
+             fill_color='YlOrRd', # 색상 변경도 가능하다
+             fill_opacity=0.7,
+             line_opacity=0.2,
+             key_on='properties.SIG_CD',
+             legend_name="지역구별 autooasis 지점 수"
+            ).add_to(m)
+
+########################################################
+
+### HEAD SETTING ###
+head1, head2 = st.columns([3,1])
+
+st.image('h1_speedMate01.png')
+st.title('자동차 현황 및 영업점 분석')
+
 # st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 
-# 대한민국 전국 지도
-# m = folium.Map(location=[38.0, 127], zoom_start=6)
-
-# # 지도 위에 마커 달기
-# folium.Marker(
-#     [37.0, 127.25],
-#     popup="Hello",
-#     tooltip="tooltip"
-
-# ).add_to(m)
-
-# st_data = st_folium(m, width = 725)
-
-# col1, col2 = st.columns([2,3])
-
-# with col1 :
-#     st.title('column 1')
-# with col2 :
-#     st.title('column2')
-#     st.checkbox('this is check box')
-
+### TAP SETTING ###
 tab1, tab2, tab3, FAQ = st.tabs(['전국 자동차 등록 현황', '전국 자동차 통행량', '영업장', 'FAQ'])
 f_faq = pd.read_csv('./result.csv')
+f_faq_c = pd.DataFrame(f_faq)
+f_faq_c = f_faq_c.drop_duplicates(['분류'])
 
+
+                    
+Faq = View(f_faq)
+data = pd.read_csv('./data/car_enrollment_year.csv', encoding='EUC-KR')
+year_ls = list(range(2018,2024))
+data2 = []
+
+for year in year_ls :
+    data2.append(data[str(year)][4])
+
+chart_data = pd.DataFrame(data2, year_ls)
 
 with tab1:
     st.header('전국 자동차 등록현황')
+    fig = plt.figure()
+    plt.bar(year_ls, data2)
+    plt.ylim([22000000,27000000])
+    st.pyplot(fig)
+
+        
+    
+
 with tab2:
     st.header('전국 자동차 통행량')
-with tab3:
-    st.header('영업장')
-with FAQ:
-    st.header('FAQ')
-    # # 2. horizontal menu
-    # selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    # icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    # menu_icon="cast", default_index=0, orientation="horizontal")
-    # selected2
-    
-    for i in range(len(f_faq['분류'])):
-        with st.expander(f_faq['분류'][i]):
-            st.write(
-                f_faq['답변'][i]
-            )
-    # st.write(f_faq)
 
-# 1. as sidebar menu
-# with st.sidebar:
-#     selected = option_menu("Main Menu", ["Side1", 'Side2'], 
-#         icons=['smile', 'house'], menu_icon="cast", default_index=1)
-#     selected
+
+
+with tab3:
+    st.header('영업장 비교')
+    col1, col2 =  st.columns([1,1])
+    with col1 :
+        st.subheader('Auto Oasis 영업장')
+        folium_static(m, width= 350, height=350)
+    with col2 : 
+        st.subheader('Auto Oasis 영업장')
+        folium_static(m, width= 350, height=350)
+
+
+with FAQ:
+    st.header('Speed Mate FAQ')
+    
+    Faq.ViewAllFAQ()
+
+    st.divider()
+
+
 
